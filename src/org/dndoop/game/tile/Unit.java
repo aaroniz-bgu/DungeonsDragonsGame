8a25b2f04b3543c18f63c07cff101129c8c8814e
Daniel Sachs <sachsd@post.bgu.ac.il>
2023-06-02 14:32:43 +0300

    Updated tile and unit

--- src/org/dndoop/game/tile/Unit.java
+++ src/org/dndoop/game/tile/Unit.java
@@ -1,9 +1,39 @@
 package org.dndoop.game.tile;
 
-public class Unit extends Tile{
+public abstract class Unit extends Tile{
+
+    private String name;
+    private Health health;
+    private UnitStats stats;
     public Unit(String name, Health health, UnitStats stats, Character character, Position position)
     {
+        super(character, position);
+        this.name = name;
+        this.health = health;
+        this.stats = stats;
+    }
 
+    protected String getName() {
+        return name;
+    }
 
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected Health getHealth() {
+        return health;
+    }
+
+    protected void setHealth(Health health) {
+        this.health = health;
+    }
+
+    protected UnitStats getStats() {
+        return stats;
+    }
+
+    protected void setStats(UnitStats stats) {
+        this.stats = stats;
     }
 }
