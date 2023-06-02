1fd2457649c6270986f8b94c82b5e63e5d6120e0
Daniel Sachs <sachsd@post.bgu.ac.il>
2023-06-02 17:34:32 +0300

    Added final static multipliers/dividors getters and setters, implemented levelUp method and increasePool, also added documentation.

--- src/org/dndoop/game/tile/Health.java
+++ src/org/dndoop/game/tile/Health.java
@@ -1,15 +1,19 @@
 package org.dndoop.game.tile;
 
 public class Health {
-
     private int healthPool;
     private int healthAmount;
+    private static final int HEALTH_POOL_MULTIPLIER = 10;
 
     public Health(int initPool) {
         this.healthAmount = initPool;
         this.healthPool = initPool;
     }
 
+    /**
+     * Regenerate the amount of health given as a parameter, capping at health pool.
+     * @param amount
+     */
     public void regen(int amount) {
         healthAmount = Math.min(healthPool, healthAmount + amount);
     }
@@ -24,19 +28,53 @@ public class Health {
         return healthAmount == 0;
     }
 
+    /**
+     * Gets health pool.
+     * @return healthPool
+     */
     public int getHealthPool() {
         return healthPool;
     }
 
+    /**
+     * Sets health pool to new
+     * @param healthPool
+     */
     public void setHealthPool(int healthPool) {
         this.healthPool = healthPool;
     }
 
+    /**
+     * Gets health amount.
+     * @return healthAmount
+     */
     public int getHealthAmount() {
         return healthAmount;
     }
 
+    /**
+     * Sets healthAmount to new healthAmount
+     * @param healthAmount
+     */
     public void setHealthAmount(int healthAmount) {
         this.healthAmount = healthAmount;
+    }
+
+    /**
+     * Increases healthPool by specified amount.
+     * @param amount
+     */
+    public void increasePool(int amount) {
+        this.healthPool += amount;
+    }
+
+    /**
+     * A method to help when a player levels up, increases their healthPool by
+     * their new level*HEALTH_POOL_MULTIPLIER and then sets their health to that amount.
+     * @param level
+     */
+    public void levelUp(int level) {
+        increasePool(HEALTH_POOL_MULTIPLIER*level);
+        setHealthAmount(healthPool);
     }
 }
