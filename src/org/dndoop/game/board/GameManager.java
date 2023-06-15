45de44e6639f0fcfcab77f0707a19b976e05da10
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-06-15 20:30:19 +0300

    first impl draft

--- src/org/dndoop/game/board/GameManager.java
+++ src/org/dndoop/game/board/GameManager.java
@@ -1,4 +1,49 @@
 package org.dndoop.game.board;
 
-public class GameManager {
+import org.dndoop.game.tile.enemies.Enemy;
+import org.dndoop.game.tile.players.Player;
+import org.dndoop.game.utils.events.GameEvent;
+import org.dndoop.game.utils.events.GameEventListener;
+
+import java.util.List;
+
+public class GameManager implements GameEventListener {
+
+    private List<String> levels;
+    private List<Enemy> enemies;
+    private GameBoard board;
+    private Player player;
+
+    private static GameManager instance;
+    private GameManager(){
+        //runGame();
+    }
+
+    /**
+     * Singleton, used to get the game's manager
+     * @return Instance of game manager.
+     */
+    public static GameManager getInstance(){
+        if(instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public void runGame() {
+
+    }
+
+    public void loadLevels() {
+
+    }
+
+    public GameBoard getGameBoard() {
+        return board;
+    }
+
+    @Override
+    public void onTick(GameEvent event) {
+
+    }
 }
