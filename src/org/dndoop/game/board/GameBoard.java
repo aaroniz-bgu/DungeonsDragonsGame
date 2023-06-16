efc1f7d45bc4a1ecdb89f5dbc49d6f3b688f53ee
Daniel Sachs <sachsd@post.bgu.ac.il>
2023-06-17 02:33:08 +0300

    Now creates a new gameboard successfully, adding all the enemies to the list of enemies.

--- src/org/dndoop/game/board/GameBoard.java
+++ src/org/dndoop/game/board/GameBoard.java
@@ -1,10 +1,16 @@
 package org.dndoop.game.board;
 
 import org.dndoop.game.tile.Tile;
+import org.dndoop.game.tile.enemies.Enemy;
+import org.dndoop.game.tile.players.Player;
 import org.dndoop.game.tile.tile_utils.Position;
 import org.dndoop.game.utils.events.GameEvent;
 import org.dndoop.game.utils.events.GameEventListener;
+import org.dndoop.game.utils.files.TileFactory;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,15 +20,65 @@ public class GameBoard implements GameEventListener, GetAtCallback {
     private final int WIDTH;
     private final int HEIGHT;
 
+    private final Character PLAYER_CHAR = '@';
+    private final Character WALL_CHAR = '#';
+
+    private List<Enemy> enemies;
+
+
     private List<Tile> board;
 
-    public GameBoard(int width, int height){
+    public GameBoard(TileFactory factory, Player player, String levelPath){
         board = new ArrayList<>();
+        int x = 1;
+        int y = 1;
+        int maxWidth = 0;
+        int maxHeight = 0;
+        enemies = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(levelPath))) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                char c = (char) character;
+                Position position = new Position(x, y);
+                switch (c) {
+                    case '.':
+                        board.add(factory.produceEmpty(position));
+                        break;
+                    case '#':
+                        board.add(factory.produceWall(position));
+                        break;
+                    case '@':
+                        player.setPosition(position);
+                        player.setTilesAccess(this);
+                        board.add(player);
+                        break;
+                    case '\r':
+                        break;
+                    case '\n':
+                        // Newline encountered, increment y, reset x.
+                        y++;
+                        maxWidth = x - 1;
+                        x = 1;
+                        break;
+                    default: //enemy
+                        Enemy enemy = factory.produceEnemy(c, position, this);
+                        enemies.add(enemy);
+                        board.add(enemy);
+                        break;
+                }
+                if (!(c == '\n' || c == '\r'))
+                    x++;
+            }
+            maxHeight = y;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.WIDTH = maxWidth;
+        this.HEIGHT = maxHeight;
+    }
 
-        this.WIDTH = width;
-        this.HEIGHT = height;
-
-        //build game board here
+    public List<Enemy> getEnemies() {
+        return enemies;
     }
 
     /**
@@ -48,8 +104,8 @@ public class GameBoard implements GameEventListener, GetAtCallback {
         String[][] build = new String[HEIGHT][WIDTH];
         for(Tile tile : board) {
             Position pos = tile.getPosition();
-            int x = pos.getX();
-            int y = pos.getY();
+            int x = pos.getX()-1;
+            int y = pos.getY()-1;
 
             build[y][x] = tile.toString();
         }
