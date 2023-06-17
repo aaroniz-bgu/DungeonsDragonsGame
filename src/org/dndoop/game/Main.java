ff3e795cc496322bc21c83c2e64fc28b3ce61ac5
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-06-17 14:37:09 +0300

    Fixed pathing

--- src/org/dndoop/game/Main.java
+++ src/org/dndoop/game/Main.java
@@ -10,7 +10,7 @@ public class Main {
         GameManager gameManager = new GameManager(System.out::println);
         Path currentDirectory = Paths.get("");
         Path parentDirectory = currentDirectory.toAbsolutePath().getParent();
-        String levels_dir = "\\levels_dir";
+        String levels_dir = "/levels_dir";
         //gameManager.start(args[0]);
         //TODO return to args[0] for jar I assume. Also can change this to match where levels_dir is in your folder
         //Its currently in the parent folder for me. We could change it so it's in the project though if you want.
